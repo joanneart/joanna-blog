559f6a38cd8a9d07720e3bd4da3ae8a5bf3d2a15
Dawidx3p <maildawcio@gmail.com>
2022-03-16 07:58:12 +0100

    change meta data for head of the document

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Navigation from "../Navigation/Navigation";
 
 import VisitCard from "../VisitCard/VisitCard";
